964eab1dfb176f417c6076f8b5781054e9e38c68
prochor666 <prochor666@gmail.com>
2017-07-31 13:59:49 +0200

    Lethe version upd

--- lib/Lethe/Config.php
+++ lib/Lethe/Config.php
@@ -308,7 +308,7 @@ class Config{
         $config['system']['productName'] = 'Lethe';
 
         // System version
-        $config['system']['version'] = '0.9.7';
+        $config['system']['version'] = '0.9.8';
 
         // Code name
         $config['system']['productCodename'] = 'Catacombs';
